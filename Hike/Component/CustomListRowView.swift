f15e19426cf12217b1537cd2c17ff7c8ae7c8350
bingbongBB <88880442+bingbongBB@users.noreply.github.com>
2024-01-09 23:02:39 +0800

    Reusable Component

---
+++ Hike/Component/CustomListRowView.swift
@@ -0,0 +1,60 @@
+//
+//  CustomListRowView.swift
+//  Hike
+//
+//  Created by 郭又萓 on 2024/1/9.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    //MARK: - PROPERTIES
+    @State var rowLabel:String
+    @State var rowIcon:String
+    @State var rowContent:String? = nil
+    @State var rowTintColor:Color
+    @State var rowLinkLabel:String? = nil
+    @State var rowLinkDestination:String? = nil
+    var body: some View {
+        LabeledContent{
+            //Content
+            if rowContent != nil {
+                Text(rowContent!)
+                    .foregroundStyle(.primary)
+                    .fontWeight(.heavy)
+            }else if(rowLinkLabel != nil && rowLinkDestination != nil){
+                Link(rowLinkLabel!,destination: URL(string:rowLinkDestination!)!)
+                    .foregroundColor(.pink)
+                    .fontWeight(.heavy)
+            }else{
+                EmptyView()
+            }
+        }label:{
+            HStack{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width:30,height:30)
+                        .foregroundColor(rowTintColor)
+                    Image(systemName:rowIcon)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                }
+                Text(rowLabel)
+            }
+        }
+    }
+}
+
+#Preview {
+    List{
+        CustomListRowView(
+            rowLabel: "Website",
+            rowIcon: "globe",
+            rowContent: nil,
+            rowTintColor: .pink,
+            rowLinkLabel: "Credo Academy",
+            rowLinkDestination: "https://credo.academy"
+        )
+    }
+    
+}
